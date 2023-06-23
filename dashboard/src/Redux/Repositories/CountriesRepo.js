aaee4ec88f89aec1359bd54027917ed3b2e3a617
Ameera Ghariba <sweet-meero@hotmail.com>
2023-06-23 16:45:28 +0300

    deploy

--- dashboard/src/Redux/Repositories/CountriesRepo.js
+++ dashboard/src/Redux/Repositories/CountriesRepo.js
@@ -8,9 +8,9 @@ import { publicRequest, userRequest } from "../../apiRequest";
 export const getCountries = async (dispatch, page, limit) => {
   dispatch(getCountryStart());
       await publicRequest.get(`country/?page=${page}&limit=${10}`).then(val => {
-            dispatch(getCountrySuccess(val.data));
+            dispatch(getCountrySuccess(val['data']));
       }).catch(err => {
-            dispatch(getCountryFailure(err.response.data));
+            dispatch(getCountryFailure(err.response['data']));
       });
 };
 
@@ -18,29 +18,29 @@ export const getCountries = async (dispatch, page, limit) => {
 export const addCountry = async (country, dispatch) => {
   dispatch(addCountryStart());
       await userRequest.post(`country/`, country ).then(val => {
-        console.log('res is ' + val.data);
-        dispatch(addCountrySuccess(val.data));
+        console.log('res is ' + val['data']);
+        dispatch(addCountrySuccess(val['data']));
       }).catch(err => {
-        dispatch(addCountryFailure(err.response.data));
+        dispatch(addCountryFailure(err.response['data']));
       });
 };
 
 export const updateCountry = async (id, Country, dispatch) => {
   dispatch(updateCountryStart());
       await userRequest.put(`country/${id}`, Country).then(val =>{
-      console.log(val.data);
-    dispatch(updateCountrySuccess(val.data));
+      console.log(val['data']);
+    dispatch(updateCountrySuccess(val['data']));
       }).catch(err=> {
-      dispatch(updateCountryFailure(err.response.data));
+      dispatch(updateCountryFailure(err.response['data']));
       });
 };
 
 export const deleteCountry = async (id, dispatch) => {
   dispatch(deleteCountryStart());
       await userRequest.delete(`country/${id}`).then(val => {
-          console.log(val.data);
+          console.log(val['data']);
     dispatch(deleteCountrySuccess(id));  
       }).catch( err => {
-       dispatch(deleteCountryFailure(err.response.data));
+       dispatch(deleteCountryFailure(err.response['data']));
       });
 };
