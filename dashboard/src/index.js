b66cbd1be066916c59db543e4dfb64a26804fe34
Ameera Ghariba <sweet-meero@hotmail.com>
2023-06-24 01:47:10 +0300

    ffff

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,13 +1,13 @@
 import { BrowserRouter} from 'react-router-dom';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import {render} from 'react-dom';
 import App from './App';
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./Redux/store";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = document.getElementById('root'); // <- This is the correct method call for React version 17
+render(
   <React.StrictMode>
 <Provider store={store}>
     <PersistGate loading="null" persistor={persistor}>      
@@ -15,7 +15,8 @@ root.render(
       <App />
     </BrowserRouter>
     </PersistGate>
-  </Provider>  </React.StrictMode>
+  </Provider>  </React.StrictMode>,
+  root
 );
 //document.getElementsByTagName('html')[0].setAttribute("dir", "rtl");
 
