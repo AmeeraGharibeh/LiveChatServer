1b4079b2660a71f093749661ff40170682549c38
Ameera Ghariba <sweet-meero@hotmail.com>
2023-07-05 10:32:20 +0300

    add

--- backend/Controllers/SearchController.js
+++ backend/Controllers/SearchController.js
@@ -3,16 +3,20 @@ const Country = require('../Models/CountryModel')
 
 const search = async (req, res)=> {
   try {
-    const { title } = req.query;
+  const keyword = req.query.search ?
+    {
+        $or: [
+        { room_name: { $regex: req.query.search, $options: "i" } },
+        { name_ar: { $regex: req.query.search, $options: "i" } },
+        { name_en: { $regex: req.query.search, $options: "i" } },
 
-  
-    const rooms = await Rooms.find({ 'room_name': title }).toArray();
-    const countries = await Country.find({
-      $or: [
-        { 'name_ar': title },
-        { 'name_en': title }
-      ]
-    }).toArray();
+        ],
+      } : {}
+    
+
+  const rooms = await Rooms.find(keyword);
+  const countries = await Rooms.find(keyword);
+
 
     if (rooms.length > 0 || countries.length > 0) {
        res.status(200).json({ rooms, countries });
@@ -21,7 +25,7 @@ const search = async (req, res)=> {
     return res.status(200).json({ message: 'No results found.' });
 
   } catch (err) {
-     res.status(500).json({ error: err.error });
+     res.status(500).json({ msg: err.message });
   }
 }
 
