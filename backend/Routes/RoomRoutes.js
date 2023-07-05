612e5af7b8490fad89237e161533ee5399b5a639
Ameera Ghariba <sweet-meero@hotmail.com>
2023-07-05 13:00:55 +0300

    mm

--- backend/Routes/RoomRoutes.js
+++ backend/Routes/RoomRoutes.js
@@ -4,10 +4,10 @@ const {createRoom, getAllRooms, getAllRoomsByCountry, getRoom,
      updateRoom, deleteRoom, searchRoom, getRoomUsers, getSpecialRooms} = require('../Controllers/RoomController')
 
 router.post('/', verifyTokenAndAuthorization, createRoom );
+router.get('/search', searchRoom );
 router.get('/', getAllRooms );
 router.get('/special', getSpecialRooms );
 router.get('/:id', getAllRoomsByCountry );
-router.get('/search', searchRoom );
 router.get('/room/:id', getRoom );
 router.get('/:id/users', getRoomUsers)
 router.put('/:id', verifyTokenAndAction, updateRoom );
