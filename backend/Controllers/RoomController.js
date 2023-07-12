66749df4b28ac295d207a87f22dd72a7daca84d9
Ameera Ghariba <sweet-meero@hotmail.com>
2023-07-12 15:01:33 +0300

    k

--- backend/Controllers/RoomController.js
+++ backend/Controllers/RoomController.js
@@ -198,11 +198,23 @@ try {
 }
 };
 
-const deleteRoom= async (req, res) => {
+const deleteRoom = async (req, res) => {
  
    try {
-         await Rooms.findByIdAndDelete(req.params.id);
-            res.status(200).json({msg: 'room has been deleted.. '});
+    await Rooms.findByIdAndDelete(req.params.id);
+    const users = await User.find({
+    room_id: req.params.id
+  });
+
+  if (users.length === 0) {
+    res.status(404).json({msg: 'no users found'});
+    return;
+  }
+  const deletedCount = await User.deleteMany({room_id: req.params.id});
+  res.status(200).json({
+    msg: `${deletedCount} users deleted`,
+  });
+    res.status(200).json({msg: 'room has been deleted.. '});
 
 } catch (err) {
     res.status(500).send({msg: err.message});
