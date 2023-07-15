bd8309c0cad4d985918e9fda5166d0d8fe879063
Ameera Ghariba <sweet-meero@hotmail.com>
2023-07-15 04:34:11 +0300

    admin

--- backend/Routes/verifyToken.js
+++ backend/Routes/verifyToken.js
@@ -3,23 +3,56 @@ const jwt = require('jsonwebtoken');
 const UserModel = require('../Models/UserModel');
 const AuthModel = require('../Models/AuthModel');
 
-const verifyToken = (req, res, next)=> {
-    const authHeader = req.headers.authorization;
-    try{
-        if(authHeader){
-        const token = authHeader.split(' ')[1];
-        jwt.verify(token, process.env.JWTSECRET, (err, user)=> {
-            if(err) res.status(401).send({msg: 'session is not valid or expired ' + err});
-            req.body = user;
-            next();
-        })
-    }else{
-        return res.status(401).send({msg: 'دخول غير مسموح'});
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  try {
+    if (authHeader) {
+      const token = authHeader.split(' ')[1];
+      jwt.verify(token, process.env.JWTSECRET, (err, user) => {
+        if (err) {
+          return res.status(401).send({ msg: 'Session is not valid or expired' });
+        }
+        req.user = user;
+        next();
+      });
+    } else {
+      return res.status(401).send({ msg: 'Unauthorized access' });
     }
-    }catch(err){
-        return res.status(500).send({msg: 'we have some glitches!'});
-    }
-}
+  } catch (err) {
+    return res.status(500).send({ msg: 'Internal server error' });
+  }
+};
+
+const verifyTokenAndAdmin = async (req, res, next) => {
+  try {
+    await verifyToken(req, res, async () => {
+      const types = ['admin', 'super_admin', 'master'];
+      const { id } = req.user;
+
+      const adminUser = await UserModel.findById(id);
+      const dashboardUser = await AuthModel.findById(id);
+
+      let master;
+
+      if (adminUser && types.includes(adminUser.user_type)) {
+        master = adminUser.username;
+        console.log('admin');
+      } else if (dashboardUser && dashboardUser.is_dashboard_admin) {
+        master = dashboardUser.username;
+        console.log('dashboard');
+      } else {
+        return res.status(403).send({ msg: 'Permission denied' });
+      }
+
+      req.body = { master, body: req.body };
+      next();
+    });
+  } catch (err) {
+    return res.status(500).send({ msg: 'Internal server error' });
+  }
+};
+
 
 const verifyTokenAndAuthorization = (req, res, next)=> {
     body = req.body;
@@ -44,29 +77,6 @@ const verifyTokenAndAuthorization = (req, res, next)=> {
   
 }
 
-const verifyTokenAndAdmin = (req, res, next)=> {
-    body = req.body;
-    let master;
-    const types = ['admin', 'super_admin', 'master']
-    verifyToken(req, res, async ()=> {
-    const adminUser = await UserModel.findById(req.body.id);
-    const dashboardUser = await AuthModel.findById(req.body.id);
-
-if(adminUser && types.includes(adminUser.user_type)) {
-    master = adminUser.username
-    console.log('admin')
-        req.body = {master, body};
-        next();    
- } else if (dashboardUser && dashboardUser.is_dashboard_admin) {
-        master = dashboardUser.username
-    console.log('dashboard')
-    req.body = {master , body};
-    next();
-} else{
-        return res.status(403).send({msg: 'عذراً, لا تملك الصلاحية للقيام هذا الإجراء'});
-    }
-    })
-}
 const verifyTokenAndAction = (req, res, next)=> {
     body = req.body
     action = req.body.action
