90ed1fed13a7afaab9e5e84c1c8c8a07babe63e7
Ameera Ghariba <sweet-meero@hotmail.com>
2023-07-22 23:41:38 +0300

    j

--- backend/Controllers/UsersController.js
+++ backend/Controllers/UsersController.js
@@ -194,7 +194,7 @@ const userLogin = async (req, res) => {
           .send({ msg: "المستخدم أو كلمة المرور غير صحيحة" });
       }
 
-      user = { ...name, room_id: member.room_id, user_type: member.user_type };
+      user = { room_id: member.room_id, user_type: member.user_type, ...name };
     } else if (req.body.room_password) {
       // Member
       user = await User.findOne({
