aae5fff9d3d2cf259445ff5e6ebc8a031bee68ba
Ameera Ghariba <sweet-meero@hotmail.com>
2023-08-13 19:42:47 +0300

    d

--- backend/Models/BlockedModel.js
+++ backend/Models/BlockedModel.js
@@ -10,6 +10,14 @@ const BlockedSchema = mongoose.Schema(
       type: String,
       required: true,
     },
+    is_ip_blocked: {
+      type: Boolean,
+      default: false,
+    },
+    is_device_blocked: {
+      type: Boolean,
+      default: false,
+    },
     master: {
       type: String,
       required: true,
