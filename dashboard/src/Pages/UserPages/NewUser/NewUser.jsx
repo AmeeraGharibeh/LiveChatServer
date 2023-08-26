d93c43bc38493e1015d53174f626419ec464b1fe
Ameera Ghariba <sweet-meero@hotmail.com>
2023-08-26 13:12:30 +0300

    as

--- dashboard/src/Pages/UserPages/NewUser/NewUser.jsx
+++ dashboard/src/Pages/UserPages/NewUser/NewUser.jsx
@@ -4,7 +4,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { addUser } from "../../../Redux/Repositories/UsersRepo";
 import { useEffect, useState } from "react";
 import { resetUserState } from "../../../Redux/UsersRedux";
-import RadioGroup from '../../../Components/RadioGroup/RadioGroup';
 import { useLocation } from "react-router-dom";
 
 
@@ -14,7 +13,6 @@ export default function NewUser() {
   const type = location.pathname.split('/')[2];
   const [room, setRoom] = useState(null);
   const [date, setDate] = useState(null);
-  const [selectedOption, setSelectedOption] = useState('');
   const rooms = useSelector((state)=> state.room.rooms);
   const success = useSelector((state) => state.user.success);
   const loading = useSelector((state) => state.user.isFetching);
@@ -22,32 +20,20 @@ export default function NewUser() {
  const dates = ["شهر", '3 أشهر', '6 أشهر', 'سنة']
   const dispatch = useDispatch();
 
-  // const options = [
-  //   { value: 'royal', label: 'اسم ملكي' },
-  //   { value: 'protected', label: 'اسم محمي' },
-  //   { value: 'root', label: 'اسم روت' },
-
-  // ];
   useEffect(()=>{
     setRoom(rooms[0]);
     setDate(dates[0]);
     console.log(type)
-    //setSelectedOption(options[0].value);
   }, []);
 
   useEffect(()=> {
     dispatch(resetUserState());
   }, []);
 
-const handleDropdownRooms = (value) => {
-  setRoom(value);
-    };
+
 const handleDropdownDate = (value) => {
   setDate(value);
     };
-      const handleOptionChange = (event) => {
-    setSelectedOption(event.target.value);
-  };
 
 
   const handleChange = (e)=> {
@@ -62,7 +48,6 @@ const handleDropdownDate = (value) => {
   const userData = { 
       ...inputs,
     name_type: type,
-   // name_end_date: date 
   };
     console.log(userData)
     addUser(userData, dispatch);
@@ -80,30 +65,12 @@ const handleDropdownDate = (value) => {
           <label>كلمة المرور</label>
           <input name="name_password" type="password"  onChange={handleChange}/>
         </div>
-  {/* {      <div className="addUserItem">
-        <label>نوع الاسم</label>
-          <RadioGroup
-        options={options}
-        selectedOption={selectedOption}
-        onChange={handleOptionChange}
-      />  
-          </div>} */}
-      {/*  <div className="addUserItem">
-          <label>الغرفة</label>
-          <DropdownMenu className="dropdown" options={rooms} value = {'room_name'} default = {room} onDropdownChange={handleDropdownRooms}/>
-        </div>
-       {    <div className="addUserItem">
-          <label>نوع المستخدم</label>
-          <DropdownMenu className="dropdown" options={types} default = {type} value = {'label'} onDropdownChange={handleDropdownUser}/>
-        </div>} */}
         <div className="addUserItem">
           <label>تاريخ الانتهاء</label>
           <DropdownMenu className="dropdown" options={dates} default = {date} onDropdownChange={handleDropdownDate}/>
         </div>
         </form>
         
-       { // <Checkbox user= {null} type= {type} onCheckboxChange={handlePermissions}/>
-       }
       <div className="addUserItem">
          <button className="addUserButton" onClick={handleClick} disabled={loading || success} >{loading ? "بالانتظار..." : success ? "تمت الاضافة" : "تأكيد" }</button>
             <div style={{color: success ? 'green' : error ? 'red' : 'black'}}>
