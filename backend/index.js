a391e2b47d422cedc1c758aca4105c899f9e1574
Ameera Ghariba <sweet-meero@hotmail.com>
2023-09-27 12:56:35 +0300

    ii

--- backend/index.js
+++ backend/index.js
@@ -32,8 +32,8 @@ const io = socketIo(server, {
 });
 dotenv.config();
 
-const appId = "104e87620771434cbfba90ac0ab35b3c";
-const appCertificate = "487dfed547d14c94a0b6edefa842a724";
+const appId = "717af643b1f44b85aa1a98fee5cdd6a1";
+const appCertificate = "401739e3af104160baf5ee08a6f4e567";
 
 ///////////////////////////////////////////////////////////
 /*app.use(cors(
