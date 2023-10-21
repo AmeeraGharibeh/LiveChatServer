0990021684e9aa60c414210cae6c2921501ac498
Ameera Ghariba <sweet-meero@hotmail.com>
2023-10-21 16:43:43 +0300

    rtt

--- backend/index.js
+++ backend/index.js
@@ -55,7 +55,7 @@ mongoose
 const onlineUsers = {};
 const clients = {};
 let log;
-
+const userSessions = {};
 io.on("connection", async (socket) => {
   console.log("A user connected");
   const ip = socket.handshake.query.ip;
@@ -63,298 +63,306 @@ io.on("connection", async (socket) => {
   const location = socket.handshake.query.location;
 
   // room joins to socket
-  socket.on("reconnect", () => {
-    socket.on("joinRoom", (room) => {
-      socket.join(room);
-      socket.room = room;
-      console.log(`User joined room: ${room}`);
-    });
-    // user joins the room
-    socket.on("addUser", async (user) => {
-      // socket.join(user._id);
-      socket.userId = user._id;
-      socket.emit("connected");
-      // send notification of user's joining the room
-      socket.broadcast.to(user.room_id).emit("notification", {
-        sender: user.username,
-        senderId: user._id,
-        message: user.username + " انضمّ إلى الغرفة",
-        color: 0xffc7f9cc,
-        type: "notification",
-      });
-
-      // update online users list and sent it to the room
-      if (!onlineUsers[user.room_id]) {
-        onlineUsers[user.room_id] = [];
-      }
-
-      if (!onlineUsers[user.room_id].includes(socket.id)) {
-        onlineUsers[user.room_id].push({ id: socket.id, user });
-        io.to(user.room_id).emit("onlineUsers", [
-          ...new Set(onlineUsers[user.room_id]),
-        ]);
-      }
-      // update room's log on db
-      const newLog = new Logs({
-        room_id: user.room_id,
-        username: user.username,
-        user_id: user._id,
-        ip,
-        device,
-        location,
-        time_in: time(),
-        icon: user.icon,
-      });
-      log = await newLog.save();
+  socket.on("joinRoom", (room) => {
+    socket.join(room);
+    socket.room = room;
+    console.log(`User joined room: ${room}`);
+  });
+  // user joins the room
+  socket.on("addUser", async (user) => {
+    // socket.join(user._id);
+    socket.userId = user._id;
+    socket.emit("connected");
+    if (userSessions[user._id]) {
+      // Reuse the existing socket session
+      const existingSocket = userSessions[user._id];
+      existingSocket.room = user.room_id;
+      // Emit any necessary events for the reconnected user
+      existingSocket.emit("reconnected", "You are reconnected.");
+    } else {
+      // Store the socket session
+      userSessions[user._id] = socket;
+    }
+    // send notification of user's joining the room
+    socket.broadcast.to(user.room_id).emit("notification", {
+      sender: user.username,
+      senderId: user._id,
+      message: user.username + " انضمّ إلى الغرفة",
+      color: 0xffc7f9cc,
+      type: "notification",
     });
 
-    socket.on("leaveRoom", async (user) => {
-      socket.leave(user.room_id);
-      //filter the list to remove left user
-      if (onlineUsers[user.room_id]) {
-        onlineUsers[user.room_id] = onlineUsers[user.room_id].filter(
-          (user) => user.id !== socket.id
-        );
-        console.log("user removed");
-      }
-      console.log(user.username + ` left room`);
+    // update online users list and sent it to the room
+    if (!onlineUsers[user.room_id]) {
+      onlineUsers[user.room_id] = [];
+    }
 
-      // Emit the updated online users list to all users in the room
+    if (!onlineUsers[user.room_id].includes(socket.id)) {
+      onlineUsers[user.room_id].push({ id: socket.id, user });
       io.to(user.room_id).emit("onlineUsers", [
         ...new Set(onlineUsers[user.room_id]),
       ]);
-      console.log(onlineUsers[user.room_id]);
-
-      io.to(user.room_id).emit("notification", {
-        sender: user.username,
-        senderId: user._id,
-        message: user.username + " غادر الغرفة  ",
-        color: 0xfffad4d4,
-        type: "notification",
-      });
-      //update room log on db
-      await Logs.updateOne({ _id: log._id }, { time_out: time() });
-      console.log("A user disconnected at " + time());
+    }
+    // update room's log on db
+    const newLog = new Logs({
+      room_id: user.room_id,
+      username: user.username,
+      user_id: user._id,
+      ip,
+      device,
+      location,
+      time_in: time(),
+      icon: user.icon,
     });
-    // handle update online users list
-    socket.on("updateOnlineUsers", (data) => {
-      if (onlineUsers[data.room_id] && socket.id) {
-        onlineUsers[data.room_id].forEach((user) => {
-          if (user.id === socket.id) {
-            user.user = data["user"];
-          }
-        });
+    log = await newLog.save();
+  });
 
-        // Emit updated online users list to all users in the room
-        io.to(data.room_id).emit("onlineUsers", [
-          ...new Set(onlineUsers[data.room_id]),
-        ]);
-      }
-    });
-
-    // Handle chat events
-    socket.on("message", (data) => {
-      console.log(
-        "Received message:",
-        data["message"] + "from: " + data["sender"]
+  socket.on("leaveRoom", async (user) => {
+    socket.leave(user.room_id);
+    //filter the list to remove left user
+    if (onlineUsers[user.room_id]) {
+      onlineUsers[user.room_id] = onlineUsers[user.room_id].filter(
+        (user) => user.id !== socket.id
       );
+      console.log("user removed");
+    }
+    console.log(user.username + ` left room`);
 
-      // Broadcast the message to all connected clients in room
-      io.to(data.room_id).emit("message", data);
+    // Emit the updated online users list to all users in the room
+    io.to(user.room_id).emit("onlineUsers", [
+      ...new Set(onlineUsers[user.room_id]),
+    ]);
+    console.log(onlineUsers[user.room_id]);
+
+    io.to(user.room_id).emit("notification", {
+      sender: user.username,
+      senderId: user._id,
+      message: user.username + " غادر الغرفة  ",
+      color: 0xfffad4d4,
+      type: "notification",
     });
-
-    // send notification when master edits the room
-    socket.on("updateRoom", (master) => {
-      io.to(master.room_id).emit("notification", {
-        sender: master.username,
-        senderId: master._id,
-        message: master.username + "  قام بتغيير اعدادات الغرفة  ",
-        color: 0xfffce9f1,
-        type: "notification",
-      });
-    });
-
-    // send notification when your profile has been visited
-    socket.on("profileVisit", (visitor) => {
-      io.to(socket.id).emit("notification", {
-        sender: visitor.username,
-        senderId: visitor._id,
-        message: visitor.username + "  قام بزيارة ملفك الشخصي  ",
-        color: 0xffffeebb,
-        type: "notification",
-      });
-    });
-    // Handle send images
-    socket.on("sendImage", (data) => {
-      const img = data.img;
-      const sender = data.sender;
-      const senderId = data.senderId;
-      const room_id = data.room_id;
-      const time = data.time;
-      const type = data.type;
-
-      // Get the image bytes from the data object.
-
-      const imageBuffer = Buffer.from(img, "base64");
-      const filename = Date.now() + ".jpg";
-
-      // Save the image to disk
-      fs.writeFile(filename, imageBuffer, (err) => {
-        if (err) {
-          console.error(err);
-          return;
+    //update room log on db
+    await Logs.updateOne({ _id: log._id }, { time_out: time() });
+    console.log("A user disconnected at " + time());
+  });
+  // handle update online users list
+  socket.on("updateOnlineUsers", (data) => {
+    if (onlineUsers[data.room_id] && socket.id) {
+      onlineUsers[data.room_id].forEach((user) => {
+        if (user.id === socket.id) {
+          user.user = data["user"];
         }
-        // Notify the client that the image has been saved.
-        io.to(room_id).emit("imageSaved", {
-          message: imageBuffer.toString(),
-          sender,
-          senderId,
-          type,
-          time,
-        });
-      });
-    });
-    // Handle private messages
-    const activeConversations = {};
-
-    socket.on("sendPrivateMessage", (data) => {
-      const friendId = data.friendId;
-      const username = data.sender;
-      const senderId = data.senderId;
-      const message = data.message;
-      const friendName = data.friendName;
-
-      // Generate a unique threadId based on the combination of senderId and friendId
-      const threadId = [senderId, friendId].sort().join("_");
-
-      console.log("thread is " + threadId);
-      // Send the private message to the recipient socket
-      if (!activeConversations[threadId]) {
-        activeConversations[threadId] = [friendId, socket.id];
-      } else if (!activeConversations[threadId].includes(friendId)) {
-        activeConversations[threadId].push(friendId);
-      }
-
-      activeConversations[threadId].forEach((socketId) => {
-        io.to(socketId).emit("privateMessage", {
-          threadId: threadId,
-          between: [
-            { sender: username, id: senderId },
-            { sender: friendName, id: friendId },
-          ],
-          message: message,
-          senderId: senderId,
-          sender: username,
-        });
       });
 
-      console.log(`${message} sent from ${username} to ${friendId}`);
+      // Emit updated online users list to all users in the room
+      io.to(data.room_id).emit("onlineUsers", [
+        ...new Set(onlineUsers[data.room_id]),
+      ]);
+    }
+  });
+
+  // Handle chat events
+  socket.on("message", (data) => {
+    console.log(
+      "Received message:",
+      data["message"] + "from: " + data["sender"]
+    );
+
+    // Broadcast the message to all connected clients in room
+    io.to(data.room_id).emit("message", data);
+  });
+
+  // send notification when master edits the room
+  socket.on("updateRoom", (master) => {
+    io.to(master.room_id).emit("notification", {
+      sender: master.username,
+      senderId: master._id,
+      message: master.username + "  قام بتغيير اعدادات الغرفة  ",
+      color: 0xfffce9f1,
+      type: "notification",
     });
+  });
 
-    // Handle reading the message
-    socket.on("messageReceived", (data) => {
-      const threadId = data.threadId;
-      io.to(socket.id).emit("messageUnread", { threadId: threadId });
+  // send notification when your profile has been visited
+  socket.on("profileVisit", (visitor) => {
+    io.to(socket.id).emit("notification", {
+      sender: visitor.username,
+      senderId: visitor._id,
+      message: visitor.username + "  قام بزيارة ملفك الشخصي  ",
+      color: 0xffffeebb,
+      type: "notification",
     });
+  });
+  // Handle send images
+  socket.on("sendImage", (data) => {
+    const img = data.img;
+    const sender = data.sender;
+    const senderId = data.senderId;
+    const room_id = data.room_id;
+    const time = data.time;
+    const type = data.type;
 
-    socket.on("messageActive", (data) => {
-      const threadId = data.threadId;
-      // Check if the conversation exists
-      // if (activeConversations[threadId]) {
-      //   activeConversations[threadId].participants.forEach((socketId) => {
-      //   });
-      // }
-      io.to(socket.id).emit("messageRead", { threadId: threadId });
-    });
+    // Get the image bytes from the data object.
 
-    // Handle user kick
-    socket.on("kickUser", async (data) => {
-      const room_id = data.room_id;
-      const user_socket = data.user_socket;
-      const master = data.master;
-      const master_id = data.master_id;
-      const username = data.username;
+    const imageBuffer = Buffer.from(img, "base64");
+    const filename = Date.now() + ".jpg";
 
-      // Remove the user from onlineUsers
-      if (onlineUsers[room_id]) {
-        onlineUsers[room_id] = onlineUsers[room_id].filter(
-          (user) => user.id !== user_socket
-        );
-        console.log("user removed");
+    // Save the image to disk
+    fs.writeFile(filename, imageBuffer, (err) => {
+      if (err) {
+        console.error(err);
+        return;
       }
-
-      // Emit the updated online users list to all users in the room
-      io.to(room_id).emit("onlineUsers", [...new Set(onlineUsers[room_id])]);
-
-      // Emit a notification to the room
-      io.to(room_id).emit("notification", {
-        sender: master,
-        senderId: master_id,
-        message: `${username} تم طرد`,
-        color: 0xfffaa2a2,
-        type: "notification",
+      // Notify the client that the image has been saved.
+      io.to(room_id).emit("imageSaved", {
+        message: imageBuffer.toString(),
+        sender,
+        senderId,
+        type,
+        time,
       });
-
-      io.to(user_socket).emit("logout", { msg: "تم طردك من الغرفة" });
     });
-    socket.on("userStatus", (data) => {
-      // Update the user's status in the onlineUsers list
-      if (onlineUsers[data.room_id] && socket.id) {
-        onlineUsers[data.room_id].forEach((user) => {
-          if (user.id === socket.id) {
-            user.user.state = data.status;
-          }
-        });
+  });
+  // Handle private messages
+  const activeConversations = {};
 
-        // Emit updated online users list to all users in the room
-        io.to(data.room_id).emit("onlineUsers", [
-          ...new Set(onlineUsers[data.room_id]),
-        ]);
-      }
+  socket.on("sendPrivateMessage", (data) => {
+    const friendId = data.friendId;
+    const username = data.sender;
+    const senderId = data.senderId;
+    const message = data.message;
+    const friendName = data.friendName;
+
+    // Generate a unique threadId based on the combination of senderId and friendId
+    const threadId = [senderId, friendId].sort().join("_");
+
+    console.log("thread is " + threadId);
+    // Send the private message to the recipient socket
+    if (!activeConversations[threadId]) {
+      activeConversations[threadId] = [friendId, socket.id];
+    } else if (!activeConversations[threadId].includes(friendId)) {
+      activeConversations[threadId].push(friendId);
+    }
+
+    activeConversations[threadId].forEach((socketId) => {
+      io.to(socketId).emit("privateMessage", {
+        threadId: threadId,
+        between: [
+          { sender: username, id: senderId },
+          { sender: friendName, id: friendId },
+        ],
+        message: message,
+        senderId: senderId,
+        sender: username,
+      });
     });
 
-    socket.on("micStatus", (data) => {
-      // Update the user's status in the onlineUsers list
-      if (onlineUsers[data.room_id] && socket.id) {
-        onlineUsers[data.room_id].forEach((user) => {
-          if (user.id === socket.id) {
-            user.user.mic_status = data.mic_status;
-          }
-        });
+    console.log(`${message} sent from ${username} to ${friendId}`);
+  });
 
-        // Emit updated online users list to all users in the room
-        io.to(data.room_id).emit("onlineUsers", [
-          ...new Set(onlineUsers[data.room_id]),
-        ]);
-      }
-    });
-    // Handle audio streaming
+  // Handle reading the message
+  socket.on("messageReceived", (data) => {
+    const threadId = data.threadId;
+    io.to(socket.id).emit("messageUnread", { threadId: threadId });
+  });
 
-    socket.on("startAudioStream", (data) => {
-      console.log("start stream data is " + data);
-      const userId = data["userId"];
-      const channelName = data["channelName"];
+  socket.on("messageActive", (data) => {
+    const threadId = data.threadId;
+    // Check if the conversation exists
+    // if (activeConversations[threadId]) {
+    //   activeConversations[threadId].participants.forEach((socketId) => {
+    //   });
+    // }
+    io.to(socket.id).emit("messageRead", { threadId: threadId });
+  });
 
-      const agoraConfig = {
-        appId: appId,
-        appCertificate: appCertificate,
-      };
-      // Generate a temporary token for the stream
+  // Handle user kick
+  socket.on("kickUser", async (data) => {
+    const room_id = data.room_id;
+    const user_socket = data.user_socket;
+    const master = data.master;
+    const master_id = data.master_id;
+    const username = data.username;
 
-      const token = agora.RtcTokenBuilder.buildTokenWithUid(
-        agoraConfig.appId,
-        agoraConfig.appCertificate,
-        channelName,
-        0,
-        agora.RtcRole.PUBLISHER,
-        3600
+    // Remove the user from onlineUsers
+    if (onlineUsers[room_id]) {
+      onlineUsers[room_id] = onlineUsers[room_id].filter(
+        (user) => user.id !== user_socket
       );
-      console.log("token is " + token);
-      // emit token to client
-      io.to(channelName).emit("streamToken", {
-        streamToken: token,
-        streamerId: userId,
+      console.log("user removed");
+    }
+
+    // Emit the updated online users list to all users in the room
+    io.to(room_id).emit("onlineUsers", [...new Set(onlineUsers[room_id])]);
+
+    // Emit a notification to the room
+    io.to(room_id).emit("notification", {
+      sender: master,
+      senderId: master_id,
+      message: `${username} تم طرد`,
+      color: 0xfffaa2a2,
+      type: "notification",
+    });
+
+    io.to(user_socket).emit("logout", { msg: "تم طردك من الغرفة" });
+  });
+  socket.on("userStatus", (data) => {
+    // Update the user's status in the onlineUsers list
+    if (onlineUsers[data.room_id] && socket.id) {
+      onlineUsers[data.room_id].forEach((user) => {
+        if (user.id === socket.id) {
+          user.user.state = data.status;
+        }
       });
+
+      // Emit updated online users list to all users in the room
+      io.to(data.room_id).emit("onlineUsers", [
+        ...new Set(onlineUsers[data.room_id]),
+      ]);
+    }
+  });
+
+  socket.on("micStatus", (data) => {
+    // Update the user's status in the onlineUsers list
+    if (onlineUsers[data.room_id] && socket.id) {
+      onlineUsers[data.room_id].forEach((user) => {
+        if (user.id === socket.id) {
+          user.user.mic_status = data.mic_status;
+        }
+      });
+
+      // Emit updated online users list to all users in the room
+      io.to(data.room_id).emit("onlineUsers", [
+        ...new Set(onlineUsers[data.room_id]),
+      ]);
+    }
+  });
+  // Handle audio streaming
+
+  socket.on("startAudioStream", (data) => {
+    console.log("start stream data is " + data);
+    const userId = data["userId"];
+    const channelName = data["channelName"];
+
+    const agoraConfig = {
+      appId: appId,
+      appCertificate: appCertificate,
+    };
+    // Generate a temporary token for the stream
+
+    const token = agora.RtcTokenBuilder.buildTokenWithUid(
+      agoraConfig.appId,
+      agoraConfig.appCertificate,
+      channelName,
+      0,
+      agora.RtcRole.PUBLISHER,
+      3600
+    );
+    console.log("token is " + token);
+    // emit token to client
+    io.to(channelName).emit("streamToken", {
+      streamToken: token,
+      streamerId: userId,
     });
   });
 
