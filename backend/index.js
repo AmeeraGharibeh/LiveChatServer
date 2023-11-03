6b610a89eb181b87a19b2f49dae436e980cc9743
Ameera Ghariba <sweet-meero@hotmail.com>
2023-11-03 11:12:41 +0300

    rtopp

--- backend/index.js
+++ backend/index.js
@@ -342,7 +342,7 @@ io.on("connection", async (socket) => {
 
     if (currentSpeaker === null) {
       currentSpeaker = userId;
-      const token = generateToken(channelName, currentSpeaker);
+      const token = generateToken(channelName, socket.id);
       console.log("token is " + token);
       io.to(channelName).emit("streamToken", {
         streamToken: token,
@@ -373,7 +373,10 @@ io.on("connection", async (socket) => {
       const nextSpeaker = speakersQueue.shift();
       currentSpeaker = nextSpeaker.userId;
       // Generate and send the token for the next speaker
-      const token = generateToken(nextSpeaker.channelName, nextSpeaker.userId);
+      const token = generateToken(
+        nextSpeaker.channelName,
+        nextSpeaker.socketId
+      );
       io.to(nextSpeaker.channelName).emit("streamToken", {
         streamToken: token,
         streamerId: currentSpeaker,
