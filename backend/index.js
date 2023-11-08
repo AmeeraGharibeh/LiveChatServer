39a6f5e7a6820dd545700aa11cf5ccb77cc50afd
Ameera Ghariba <sweet-meero@hotmail.com>
2023-11-08 10:38:54 +0300

    jiji

--- backend/index.js
+++ backend/index.js
@@ -187,7 +187,7 @@ io.on("connection", async (socket) => {
       "Received message:",
       data["message"] + "from: " + data["sender"]
     );
-    if (ignoredUsers.has(data["senderId"])) {
+    if (!ignoredUsers.has(data["senderId"])) {
       socket.broadcast.to(data.room_id).emit("message", data);
     } else {
       io.to(data.room_id).emit("message", data);
