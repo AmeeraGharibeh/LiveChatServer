759755d075269465570c7949d14d92dc76721a8a
Ameera Ghariba <sweet-meero@hotmail.com>
2024-01-09 12:29:21 +0300

    fix_upload_route

--- backend/Routes/UploadRoutes.js
+++ backend/Routes/UploadRoutes.js
@@ -140,7 +140,7 @@ async function uploadImagesToBucket(directory, files) {
 
     const uploadPromises = files.map(async (file) => {
       const timestamp = Date.now();
-      const uniqueFilename = `${timestamp}_${file.originalname.replace(
+      const uniqueFilename = `${directory}${timestamp}_${file.originalname.replace(
         / /g,
         "_"
       )}`;
@@ -156,7 +156,7 @@ async function uploadImagesToBucket(directory, files) {
         });
 
         blobStream.on("finish", async () => {
-          const publicUrl = `https://storage.googleapis.com/${bucket.name}/${directory}/${blob.name}`;
+          const publicUrl = `https://storage.googleapis.com/${bucket.name}/${blob.name}`;
           try {
             await blob.makePublic();
             resolve({ filename: uniqueFilename, url: publicUrl });
