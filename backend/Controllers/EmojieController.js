e13f10d399613fc93af97c598a7cce9e3ec363f4
Ameera Ghariba <sweet-meero@hotmail.com>
2024-01-28 00:06:02 +0300

    delete_emoji_caategory

--- backend/Controllers/EmojieController.js
+++ backend/Controllers/EmojieController.js
@@ -30,6 +30,22 @@ const deleteEmojie = async (req, res) => {
     res.status(500).send({ msg: err.message });
   }
 };
+
+const deleteEmojieByCategory = async (req, res) => {
+  try {
+    const emojisToDelete = await Emojie.find({ category: req.params.category });
+
+    // Delete all found emojis
+    await Emojie.deleteMany({ category: req.params.category });
+
+    res.status(200).json({
+      msg: `تم حذف ${emojisToDelete.length} ايموجي من التصنيف${req.params.category}`,
+      deletedEmojis: emojisToDelete,
+    });
+  } catch (err) {
+    res.status(500).send({ msg: err.message });
+  }
+};
 module.exports = {
   getEmojies,
   deleteEmojie,
