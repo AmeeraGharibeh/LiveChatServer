2cf26c115f0bd1ba0ed6ecefb7c00e3249c9d29b
Ameera Ghariba <sweet-meero@hotmail.com>
2024-03-04 23:17:14 +0300

    fix_list

--- backend/index.js
+++ backend/index.js
@@ -545,19 +545,18 @@ io.on("connection", async (socket) => {
   });
 
   socket.on("stopAudioStream", (data) => {
-    console.log("speakers " + JSON.stringify(speakersQueue));
-    let indexToRemove = speakersQueue[data["roomId"]].findIndex(
-      (item) => item.socketId === data["socketId"]
-    );
-    if (indexToRemove !== -1) {
-      speakersQueue[data["roomId"]].splice(indexToRemove, 1);
-    }
     updateOnlineUsersList(data.roomId, data["socketId"], "mic_status", "none");
     socket.emit("endStreaming");
     onlineUsers[data.roomId].forEach((user) => {
       user.user["audio_status"] = "none";
     });
     if (speakersQueue[data.roomId].length > 0) {
+      let indexToRemove = speakersQueue[data["roomId"]].findIndex(
+        (item) => item.socketId === data["socketId"]
+      );
+      if (indexToRemove !== -1) {
+        speakersQueue[data["roomId"]].splice(indexToRemove, 1);
+      }
       startStreaming(speakersQueue[data.roomId][0]);
     } else {
       speakersQueue[data.roomId] = [];
