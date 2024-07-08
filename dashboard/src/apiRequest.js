e894eb5f23d7ee0caa246c80169da58e5bca0541
AmeeraGharibeh <56909303+AmeeraGharibeh@users.noreply.github.com>
2024-07-08 10:50:36 +0300

    Update apiRequest.js

--- dashboard/src/apiRequest.js
+++ dashboard/src/apiRequest.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "https://audio-video-chat-backend.onrender.com/";
+const BASE_URL = "https://syriachatserver.onrender.com/";
 
 // Define a function to retrieve the token from local storage.
 const getTokenFromLocalStorage = () => {
